a468c4ebcfd8841f5a23057a2f01c1fd616a0f8d
Karlis Lauva <k@karlis.me>
2019-07-22 17:10:02 +0300

    basic coordinate parsing

--- src/geotools/format.clj
+++ src/geotools/format.clj
@@ -1,5 +1,6 @@
 (ns geotools.format
-  (:require [geotools.core :refer [to-ddm]]))
+  (:require [clojure.string :as str]
+            [geotools.core :refer [to-ddm]]))
 
 (defn format-geocheck
   "Formats given {:lat XXX :lon XXX} map for GeoCheck.org validation."
@@ -12,3 +13,27 @@
          (if (pos? lat-deg) "E" "W")
          (format "%03d " (Math/abs lon-deg))
          (format "%06.3f" lon-min))))
+
+;;; Parsing
+
+(def ^:private multipliers {"N" 1 "S" -1 "E" 1 "W" -1})
+
+(defn- parse-coordinate [multiplier [d m? s?]]
+  (cond
+    s? [(* multiplier (Integer. d)) (Integer. m?) (Double. s?)]
+    m? [(* multiplier (Integer. d)) (Double. m?)]
+    :else (* multiplier (Double. d))))
+
+(defn parse-string
+  "Attempts to parse given lat/lon coordinate string. Returns a lat lon
+   map with the same cordinate format that was provided in input on success."
+  [raw-str]
+  (let [multipliers (->> (re-matches #".*([NS]).*([EW]).*" raw-str) rest
+                         (map multipliers))
+        split-str (-> (str/replace raw-str #"[A-Z]" "")
+                      (str/trim)
+                      (str/split #"[\s\"\'°′″,]+"))]
+    (->> (split-at (/ (count split-str) 2) split-str)
+         (map parse-coordinate (or (seq multipliers) [1 1]))
+         (interleave [:lat :lon])
+         (apply hash-map))))
--- test/geotools/format_test.clj
+++ test/geotools/format_test.clj
@@ -9,8 +9,45 @@
 
   (is (= (format-geocheck {:lat 54.075833 :lon 29.301605})
          "N54 04.550 E029 18.096")
-      "Latituded is 0-padded")
+      "Latitude is 0-padded")
 
   (is (= (format-geocheck {:lat -12.511979 :lon -53.807635})
          "S12 30.719 W053 48.458")
       "Negative degrees are formatted as expected"))
+
+(deftest string-parsing
+  (testing "Google Maps formats"
+    (is (= (parse-string "56°55'25.5\"N 24°05'52.9\"E")
+           {:lat [56 55 25.5] :lon [24 5 52.9]}))
+
+    (is (= (parse-string "56.921758, 24.147614")
+           {:lat 56.921758 :lon 24.147614}))
+
+    (is (= (parse-string "40°49'50.9\"N 73°47'48.0\"W")
+           {:lat [40 49 50.9] :lon [-73 47 48.0]}))
+
+    (is (= (parse-string "-27.116790, -109.369032")
+           {:lat -27.116790 :lon -109.369032})))
+
+  (testing "Wikipedia formats"
+    (is (= (parse-string "54.652°N 24.934°E") {:lat 54.652 :lon 24.934}))
+
+    (is (= (parse-string "37°14′0″N 115°48′30″W")
+           {:lat [37 14 0.0] :lon [-115 48 30.0]}))
+
+    (is (= (parse-string "40°38′23″N 073°46′44″W")
+           {:lat [40 38 23.0] :lon [-73 46 44.0]})))
+
+  (testing "Reverse GeoCheck parsing"
+    (is (= (parse-string "N56 55.008 E024 05.696")
+           {:lat [56 55.008] :lon [24 5.696]}))
+
+    (is (= (parse-string "N54 04.550 E029 18.096")
+           {:lat [54 4.550] :lon [29 18.096]}))
+
+    (is (= (parse-string "S12 30.719 W053 48.458")
+           {:lat [-12 30.719] :lon [-53 48.458]})))
+
+  (testing "Geocaching format"
+    (is (= (parse-string "N 56° 56.488 E 024° 06.500")
+           {:lat [56 56.488] :lon [24 6.5]}))))
